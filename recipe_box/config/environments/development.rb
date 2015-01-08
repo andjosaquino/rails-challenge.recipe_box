f8a19f02bb12c240f5f1f7878377df1548266946
Andrew Aquino <aja255@cornell.edu>
2015-01-08 18:59:14 -0500

    Finished. Thank You Mackenzie Child!

--- recipe_box/config/environments/development.rb
+++ recipe_box/config/environments/development.rb
@@ -40,4 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
   Paperclip.options[:command_path] = "C:\Program Files\ImageMagick-6.9.0-Q16"
+
+  #DEVISE
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
